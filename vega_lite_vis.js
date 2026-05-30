4d88e540c34279b024c23e18da737fdcee447546
mimr0013 <mimr0013@student.monash.edu>
2026-05-30 20:30:28 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -11,17 +11,17 @@ vegaEmbed("#symbolmap", vg_3).then(function(result) {
 }).catch(console.error);
 
 var vg_4 = "heatmap_bowlers.vg.json";
-vegaEmbed("#lollipop", vg_1).then(function(result) {
+vegaEmbed("#heatmap_bowlers", vg_4).then(function(result) {
 }).catch(console.error);
 
 var vg_5 = "diverging_opp.vg.json";
-vegaEmbed("#linechart", vg_2).then(function(result) {
+vegaEmbed("#diverging_opp", vg_5).then(function(result) {
 }).catch(console.error);
 
 var vg_6 = "choropleth_opp.vg.json";
-vegaEmbed("#symbolmap", vg_3).then(function(result) {
+vegaEmbed("#choropleth_opp", vg_6).then(function(result) {
 }).catch(console.error);
 
 var vg_7 = "captains_ribbon.vg.json";
-vegaEmbed("#symbolmap", vg_3).then(function(result) {
+vegaEmbed("#captains_ribbon", vg_7).then(function(result) {
 }).catch(console.error);
